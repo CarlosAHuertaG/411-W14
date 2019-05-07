59b89be3534572120f036080186348f67dbd34be
Carlos Huerta <charlyhg9597@gmiail.com>
2019-05-07 14:08:08 +0300

    Files

--- Python3_Server.py
+++ Python3_Server.py
@@ -1,13 +1,39 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+# -*- coding: utf-8 -*-
+# Ejemplo cliente - servidor en python
+# Programa Servidor
+# www.elfreneticoinformatico.com
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+#Importacines:
+#utilidades de red y conexion
+import socket
 
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
+#Definimos parametros necesarios por defeccto
+ip = "0.0.0.0"
+puerto = 8000
+dataConection = (ip, puerto)
+conexionesMaximas = 5 #Podrán conectarse 5 clientes como máximo
 
+#Creamos el servidor.
+#socket.AF_INET para indicar que utilizaremos Ipv4
+#socket.SOCK_STREAM para utilizar TCP/IP (no udp)
+socketServidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+socketServidor.bind(dataConection) #Asignamos los valores del servidor
+socketServidor.listen(conexionesMaximas) #Asignamos el número máximo de conexiones
+
+print("Esperando conexiones en %s:%s" %(ip, puerto))
+cliente, direccion = socketServidor.accept()
+print("Conexion establecida con %s:%s" %(direccion[0], direccion[1]))
+
+#Bucle de escucha. En él indicamos la forma de actuar al recibir las tramas del cliente
+while True:
+    datos = cliente.recv(4096) #El número indica el número maximo de bytes
+    if datos == "exit":
+        cliente.send("exit")
+        break
+    print("RECIBIDO: %s" %datos)
+    cliente.sendall("-- Recibido --")
+    
+print("------- CONEXIÓN CERRADA ---------")
+socketServidor.close()
